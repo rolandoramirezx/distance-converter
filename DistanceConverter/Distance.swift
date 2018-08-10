5042abdb84805ac6224b41a97045c090d32f2e7c
Rolando Ramirez <rolando.ramirezx@gmail.com>
2018-08-10 13:18:05 -0500

    created Distance class

---
+++ DistanceConverter/Distance.swift
@@ -0,0 +1,30 @@
+//
+//  Distance.swift
+//  DistanceConverter
+//
+//  Created by Rolando Ramirez on 8/9/18.
+//  Copyright © 2018 Rolando Ramirez. All rights reserved.
+//
+
+import Foundation
+
+class Distance {
+    
+    static let kmPerMile = 1.60934
+    var miles:Double
+    var km:Double{
+        return Distance.toKm(miles:miles)
+    }
+    init(miles:Double){
+        self.miles = miles
+    }
+    init(km:Double){
+        self.miles = Distance.toMiles(km:km)
+    }
+    static func toKm(miles:Double) -> Double {
+        return miles * kmPerMile
+    }
+    static func toMiles(km:Double) -> Double {
+        return km / kmPerMile
+    }
+}
